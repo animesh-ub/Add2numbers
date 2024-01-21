0dec6a3fc474f196f325661890db90c8a5419b4d
AnimeshPandey <AnimeshPandey@GEDU-LP350>
2024-01-22 02:02:40 +0530

    Add project files.

---
+++ Add2numbers/Controllers/AddtwonumbersController.cs
@@ -0,0 +1,24 @@
+﻿using Add2numbers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Add2numbers.Controllers
+{
+    public class AddtwonumbersController : Controller
+    {
+        public class Add2NumController : Controller
+        {
+            [HttpGet]
+            public IActionResult Index()
+            {
+                Numbers number = new Numbers();
+                return View(number);
+            }
+            [HttpPost]
+            public IActionResult Index(Numbers numbers)
+            {
+                ViewBag.res = numbers.Num1 + numbers.Num2;
+                return View();
+            }
+        }
+    }
+}
